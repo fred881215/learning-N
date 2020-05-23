0c55fb3b0052eb9eea7159d12b95c7b44530af12
fred881215 <fred881215@gmail.com>
2020-05-23 12:59:39 +0800

    0523

--- django/django_0519/ithome/vendor/urls.py
+++ django/django_0519/ithome/vendor/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('welcome/', include('welcome.urls')),
-    path('vendor/', include('vendor.urls')),
+    path('', views.vendor_index, name="vendor_index"),
+    path('create', views.vendor_create_view),
 ]
--- django/django_0519/ithome/vendor/views.py
+++ django/django_0519/ithome/vendor/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Vendor
+from .forms import VendorForm
 
 # Create your views here.
+def vendor_index(request):
+    vendor_list = Vendor.objects.all() # 把所有 Vendor 的資料取出來
+    context = {'vendor_list': vendor_list} # 建立 Dict對應到Vendor的資料，
+    return render(request, 'vendor/vendor_detail.html', context)
+
+# 針對 vendor_create.html
+def vendor_create_view(request):
+    form = VendorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = VerdorForm() # 清空 form
+
+    context = {
+        'form' : form
+    }
+    return render(request, "vendor/vendor_create.html", context)
