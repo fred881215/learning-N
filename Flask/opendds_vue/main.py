81b64ec2dd682e505f847febe997edd289ca4a4c
fred881215 <fred881215@gmail.com>
2020-10-20 17:05:55 +0800

    main

--- Flask/opendds_vue/main.py
+++ Flask/opendds_vue/main.py
@@ -571,4 +571,4 @@ if __name__ == '__main__':
     app.debug = True
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
